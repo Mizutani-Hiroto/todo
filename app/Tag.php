be349e61ba2bbd34dd6ba5b44be2679b4bd8f0c9
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2021-02-17 11:19:32 +0000

    add relation

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function todos()
+    {
+        return $this->belongsToMany('App\Todo');
+    }
 }
